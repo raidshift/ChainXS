443e400db6e2bcec05e7e4f0dab96a299327d82f
raidshift <laurenz.zielinski@gmail.com>
2023-10-13 21:44:20 +0200

    build_29

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.7"),
         .package(url: "https://github.com/bitflying/SwiftKeccak.git", from: "0.1.0"),
-        .package(url: "https://github.com/raidshift/noxs", branch: "main"),
+        .package(url: "https://github.com/raidshift/noxs", from: "1.1.0"),
     ],
     targets: [
         .executableTarget(
